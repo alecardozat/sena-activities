1b1edc01546c7a7ac7f4706617589ca3e843e5e2
alejandroct21 <alecardozat@gmail.com>
2026-05-25 13:03:41 -0500

    fix: corregido ejercicio01 en rust

--- GA3-220501093-AA3-EV02/rust/ejercicio01/src/main.rs
+++ GA3-220501093-AA3-EV02/rust/ejercicio01/src/main.rs
@@ -28,80 +28,40 @@ fn main() {
             },
             _ => println!("No es un valor válido\n")
         }
-
     }
 }
 
 fn show_circle(){
     println!("Escogiste CIRCULO\n");
     println!("Ingresa el radio: ");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("error input circulo");
-
-    let radio:u16 = match input.trim().parse(){
-        Ok(num) => num,
-        Err(_) => {
-            println!("valor invalido");
-            return;
-        },
-    };
+    let radio:u16 = read_number();
 
     let area = PI * (radio as f64).powi(2);
     let perimeter = 2.0 * PI * (radio as f64).powi(2);
 
     println!("perimeter: {perimeter}\narea: {area}\n");
-
 }
 
 fn show_square(){
     println!("Escogiste CUADRADO\n");
+
     println!("Ingresa el lado: ");
+    let lado = read_number();
 
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("error input cuadrado");
-
-    let lado:u16 = match input.trim().parse(){
-        Ok(num)=>num,
-        Err(_) => {
-            println!("valor invalido");
-            return;
-        },
-    };
-
-    let area = 4 * lado;
-    let perimeter = (lado as f64).powi(2);
+    let area = (lado as f64).powi(2);
+    let perimeter = 4 * lado;
 
     println!("perimeter: {perimeter}\narea: {area}\n");
 }
 
 fn show_rectangle(){
     println!("Escogiste RECTANGULO\n");
+
     println!("Ingresa el lado a: ");
-    let mut input = String::new();
-
-    io::stdin().read_line(&mut input).expect("error ladoA rectangulo");
-
-    let lado_a:u16 = match input.trim().parse(){
-        Ok(num) => num,
-        Err(_) => {
-            println!("no es un valor válido");
-            return;
-        }
-    };
+    let lado_a = read_number();
 
     println!("Ingresa el lado b: ");
-
-    let mut input = String::new();
-
-    io::stdin().read_line(&mut input).expect("error ladoB rectangulo");
-
-    let lado_b:u16 = match input.trim().parse(){
-        Ok(num) => num,
-        Err(_) => {
-            println!("no es un valor válido");
-            return;
-        }
-    };
+    let lado_b = read_number();
 
     let perimeter = 2 * (lado_b + lado_a);
     let area = lado_a * lado_b; 
@@ -113,51 +73,33 @@ fn show_triangle(){
     println!("Escogiste TRIANGULO\n");
 
     println!("Ingresa la base: ");
-    let mut input_b = String::new();
-    io::stdin().read_line(&mut input_b).expect("error base triangulo");
-    let base:i16 = match input_b.trim().parse(){
-        Ok(num) => num,
-        Err(_) => {
-            println!("no es un valor válido");
-            return;
-        }
-    };
+    let base = read_number();
 
     println!("Ingresa la altura: ");
-    let mut input_h = String::new();
-    io::stdin().read_line(&mut input_h).expect("error altura triangulo");
-    let altura:i16 = match input_h.trim().parse(){
-        Ok(num) => num,
-        Err(_) => {
-            println!("no es un valor válido");
-            return;
-        }
-    };
+    let altura = read_number();
 
     println!("Ingresa lado a: ");
-    let mut input_a = String::new();
-    io::stdin().read_line(&mut input_a).expect("error lado a triangulo");
-    let lado_a:i16 = match input_a.trim().parse(){
-        Ok(num) => num,
-        Err(_) => {
-            println!("no es un valor válido");
-            return;
-        }
-    };
+    let lado_a = read_number();
 
     println!("Ingresa lado c: ");
-    let mut input_c = String::new();
-    io::stdin().read_line(&mut input_c).expect("error lado c triangulo");
-    let lado_c:i16 = match input_c.trim().parse(){
-        Ok(num) => num,
-        Err(_) => {
-            println!("no es un valor válido");
-            return;
-        }
-    };
+    let lado_c = read_number();
 
     let area = (base * altura)/2;
     let perimeter = lado_a + base + lado_c; 
 
     println!("perimeter: {perimeter}\narea: {area}\n");
+}
+
+fn read_number()->u16{
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("error input");
+
+    let input:u16 = match input.trim().parse(){
+        Ok(num) => num,
+        Err(_) => {
+            println!("valor invalido");
+            0
+        }
+    };
+    input
 }
